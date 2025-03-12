c3690f9e8f55812273927e2c31f23045d8bbf76e
gaomengying <64011694+xiaoni1@users.noreply.github.com>
2025-03-12 18:56:16 +0800

    引入elementplus和sass定义化颜色覆盖

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,38 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
+// 按需导入element-plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      //1、 配置elementPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2、自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `
+      }
+    }
+  }
 })
