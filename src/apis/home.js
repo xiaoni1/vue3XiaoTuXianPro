6981878e4881ba9f4a5cd8a09d1c0cda7e0b8578
gaomengying <1289354175@qq.com>
2025-03-17 00:48:54 +0800

    首页新鲜好物&人气推荐

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,26 @@
 import httpInstance from '@/utils/http'
 
+// 获取banner列表
 export function getBannerAPI() {
     return httpInstance({
         url: '/home/banner'
     })
 }
+
+// 获取新鲜好物
+export function findNewAPI() {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+    return httpInstance({
+      url: '/home/hot'
+    })
+  }
