92a09fbd9d67f7aeda6ba5122abe77540763288e
gaomengying <1289354175@qq.com>
2025-03-26 18:13:54 +0800

    一级分类路由配置

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -15,7 +15,7 @@ const categoryStore = useCategoryStore()
 
             <ul class="app-header-nav">
                 <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-                    <RouterLink to="/">{{ item.name }}</RouterLink>
+                    <RouterLink :to="`/category/${ item.id }`">{{ item.name }}</RouterLink>
                 </li>
             </ul>
 
